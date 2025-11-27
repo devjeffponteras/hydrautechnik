3c2440f9f8de0c4c4f43a7d49b47fc80de467f35
Jeff Ponteras <jeffponteras.pmc@gmail.com>
2025-11-27 08:49:48 +0800

    top bar elements

--- resources/views/theme/layouts/components/menu.blade.php
+++ resources/views/theme/layouts/components/menu.blade.php
@@ -8,4 +8,10 @@
     @foreach ($menu->parent_navigation() as $item)
         @include('theme.layouts.components.menu-item', ['item' => $item])
     @endforeach
+
+    <!-- small screen call us btn -->
+    <a href="#" class="call-us-wide-btn-sm btn btn-sm btn-warning text-dark d-flex align-items-center mb-2 gap-2" style="font-size: 12px">
+        <i class="icon-line-arrow-right mr-2" style="font-size: 12px"></i>
+        Call us: {{ optional(App\Helpers\Setting::info())->mobile_no ?? '' }}
+    </a>
 </ul>
